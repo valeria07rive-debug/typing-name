bd137341aa1b2509c60f47c1b87b9be8182b7ad2
valeria07rive-debug <valeria07rive@gmail.com>
2026-02-17 11:11:04 -0400

    Agrega basic typing game logic

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello typing game")
+import random
+
+# lista de palabras
+palabras = [
+    "elver", "jagua", "cafe", "keyboard", "otorinolaringologia",
+    "donaty", "magnanimo", "naruto", "discrepancia", "transeunte",
+    "camila", "badbunny", "soto", "braulio", "notengounarealmente"
+]
+
+# seleccionar 15 palabras aleatorias sin repetir
+palabras_seleccionadas = random.sample(palabras, 15)
+
+errores = 0
+
+for palabra in palabras_seleccionadas:
+    print("\nEscribe esta palabra:", palabra)
+    entrada_usuario = input("> ")
+
+    if entrada_usuario == palabra:
+        print("¡Correcto!")
+    else:
+        print("Incorrecto")
+        errores += 1
+
+print("\nFin del juego")
+print("Errores:", errores)
+
+
+
