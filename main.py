278ae3ba6225a2b1a8fbd8a8679b81cdb0f55f4a
valeria07rive-debug <valeria07rive@gmail.com>
2026-02-27 00:53:43 -0400

    improvee

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
 import random
 
-
 palabras = [
     "elver", "jagua", "cafe", "estupefacto", "otorinolaringologia",
     "donaty", "magnanimo", "naruto", "discrepancia", "transeunte",
     "camila", "badbunny", "soto", "braulio", "notengounarealmente"
 ]
 
-
 palabras_seleccionadas = random.sample(palabras, 15)
 
 errores = 0
 correctas = 0
-
 
 for palabra in palabras_seleccionadas:
     print("\nEscribe esta palabra:", palabra)
     entrada_usuario = input("> ")
 
     if entrada_usuario == palabra:
-    print("¡Correcto!")
-    correctas += 1
-else:
-    print("Incorrecto")
-    errores += 1
-
+        print("¡Correcto!")
+        correctas += 1
+    else:
+        print("Incorrecto")
+        errores += 1
 
 print("\nFin del juego")
-print("Errores:", errores)
 
 total = len(palabras_seleccionadas)
 accuracy = (correctas / total) * 100
 
 print("Palabras correctas:", correctas)
 print("Errores:", errores)
-print(f"Exactitud final: {accuracy:.2f}%")
-
+print(f"Exactitud final: {accuracy:.2f}%") 
 
 
 
